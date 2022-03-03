1b9cd033fa23771448985465d1b8c30f87f1ff47
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2022-03-03 23:03:41 +0700

    [ADD] TripsContext and TableContent inorder to search and display results

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './index.css';
 import Searchbar from '../components/Searchbar';
+import TripsContext from '../context/ManageTrips/tripsContext';
+import TableContent from '../components/TableContent';
 
 const Home = () => {
   const [showData, setShowData] = useState([])
+  const tripsContext = useContext(TripsContext);
+  const { getTrips, trips } = tripsContext;
 
   useEffect(() => {
-    if (showData) {
-      console.log('SUCCESS:', showData)
-      console.log(document.getElementById('Searchbar').value)
-    }
-  }, [showData])
+    getTrips({ keyword: ""});
+  }, [getTrips]);
 
+  useEffect(() => {
+    if (trips) {
+      setShowData(trips);
+    }
+  }, [trips]);
+
+  useEffect(() => {
+    console.log(showData)
+  }, [showData])
 
   return (
     <div className="Home">
         <header className="Home-header">
             <p className="Home-title">เที่ยวไหนดี</p>
-            <Searchbar setShowData={setShowData}/>
+            <Searchbar/>
+            <TableContent data={showData}/>
             <a
             className="Home-link"
             href="https://reactjs.org"
