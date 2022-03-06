9130b196e88888ace9825eac3b68af8560fa61ef
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2022-03-06 12:36:48 +0700

    [MODIFY] catch errors and statuses

--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -4,27 +4,31 @@ const axios = require('axios')
 const registry = require('./registry.json')
 
 router.all('/:apiName/:path', (req, res) => {
-    if (registry.services[req.params.apiName]) {
-        if (req.params.path === "trips") {
-            let keyword = req.query.keyword;
-            axios({
-                method: req.method,
-                url: registry.services[req.params.apiName].url + req.params.path,
-                headers: req.headers,
-                data: req.body
-            }).then((respond) => {
-                let data = respond.data;
-                let data_filter = data.filter(element => element.title.includes(keyword) || element.description.includes(keyword) || element.tags.includes(keyword))
-                res.status(200).send(data_filter)
-            })
-            .catch((err) => {
-                res.send(err)
-            })
+    try {
+        if (registry.services[req.params.apiName]) {
+            if (req.params.path === "trips") {
+                const keyword = req.query.keyword || "";
+                axios({
+                    method: req.method,
+                    url: registry.services[req.params.apiName].url + req.params.path,
+                    headers: req.headers,
+                    data: req.body
+                }).then((respond) => {
+                    const data = respond.data;
+                    const data_filter = data.filter(element => element.title.includes(keyword) || element.description.includes(keyword) || element.tags.includes(keyword))
+                    res.status(200).send(data_filter)
+                })
+                .catch((err) => {
+                    res.status(400).send(err)
+                })
+            } else {
+                res.status(404).send("Path Name Doesn't Exist");
+            }
         } else {
-            res.send("Path Name doesn't Exist")
+            res.status(404).send("API Name Doesn't Exist");
         }
-    } else {
-        res.send("API Name doesn't Exist")
+    } catch (err) {
+        res.status(500).send(err);
     }
 })
 
